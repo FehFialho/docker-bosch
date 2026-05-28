c7410882cab7d6374697d4ffe428a0ef7b2b76a8
FehFialho <fernandaklechowicz@gmail.com>
2026-05-28 13:38:09 -0300

    feat: add contents and exercises

---
+++ 1. Intro/httyd/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+// New Commnent
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/getDragon", () =>
+{
+    return "Toothless";
+})
+.WithName("GetDragon");
+
+app.Run();
