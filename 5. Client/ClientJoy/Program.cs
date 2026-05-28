7ed1ebfa5e798a32e4fdfeb8a855589b5d265d40
FehFialho <fernandaklechowicz@gmail.com>
2026-05-28 16:51:13 -0300

    feat: add nginx with clients

---
+++ 5. Client/ClientJoy/Program.cs
@@ -0,0 +1,17 @@
+﻿var http = new HttpClient();
+
+System.Console.WriteLine("Cliente Joy Iniciada");
+
+while (true)
+{
+    try
+    {
+        var response = await http.GetAsync("http://maestro:8000/aula");
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Error: {ex.Message}");
+    }
+    Thread.Sleep(1000);
+}
